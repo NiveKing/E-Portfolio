53e051f968d72dd70fd53e303c5df0c0d644e5c5
Kevin Abaquita <nivekbusinesses@gmail.com>
2023-08-04 22:30:17 +0800

    Working on workcard

--- src/Routes/Projects.jsx
+++ src/Routes/Projects.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import Hero2 from '../Components/Heroimg2';
+import Workcard from '../Components/Workcard';
 
 const Projects = () => {
     return (
         <div>
             <Navbar />
             <Hero2 heading='Projects' text='Here are some of my works' />
+            <Workcard />
             <Footer />
         </div>
     )
