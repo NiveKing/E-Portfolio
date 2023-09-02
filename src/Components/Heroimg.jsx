ea0bc17d05300bfe28f1513c8dbbbfee0863f10d
Kevin Abaquita <nivekbusinesses@gmail.com>
2023-09-02 16:01:24 +0800

    changed tab icon and role to web developer

--- src/Components/Heroimg.jsx
+++ src/Components/Heroimg.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             </div>
             <div className="content">
                 <p>Hi, Im Kevin Abaquita</p>
-                <h1>React Developer</h1>
+                <h1>Web Developer</h1>
                 <div>
                     <Link to='/projects' className='btn'>Projects</Link>
                     <Link to='/contact' className='btn btn-light'>Contact</Link>
