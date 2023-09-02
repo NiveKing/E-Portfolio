a6b2b53495197eb99a512429a31af7c977f11be4
Kevin Abaquita <nivekbusinesses@gmail.com>
2023-09-03 02:29:03 +0800

    added another project

--- src/Components/WorkData.jsx
+++ src/Components/WorkData.jsx
@@ -6,7 +6,7 @@ const ProjectData = [
     {
         imgsrc: proj1,
         title: 'To do list',
-        text: 'This is my first project, all codes are my own. This the first time i used what i learnt. It is simple and messy but the code worked',
+        text: 'This is my first project, all codes are my own. This the first time i used what i learnt. It is simple and a bit messy.',
         view: 'https://github.com/NiveKing/Todolist'
     },
     {
@@ -15,6 +15,12 @@ const ProjectData = [
         text: 'This is a copied work from youtube, but I modified it a little bit because the api used in the video was outdated',
         view: 'https://github.com/NiveKing/weatherApp'
     },
+    {
+        imgsrc: proj3,
+        title: 'Login Authentication',
+        text: 'My first project with back-end. Learnt Express JS, Database(SQL) and how to use and create APIs.',
+        view: 'https://github.com/NiveKing/loginAuthentication'
+    }
 ]
 
 export default ProjectData
